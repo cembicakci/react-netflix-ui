1cb886ac2ae15c5ef806c9e70fe27c9a5d2b494e
Cem <cmbicakci@gmail.com>
2022-11-23 20:19:24 +0300

    installed material-ui-icons

---
+++ src/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './home.scss'
+import { AcUnitIcon } from '@mui/icons-material';
+
+
+function Home() {
+    return (
+        <div className='home'>
+            <AcUnitIcon />
+        </div>
+    )
+}
+
+export default Home
