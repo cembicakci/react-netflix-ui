2a01b1486c26a558a732abf462fb52e08a7e3822
Cem <cmbicakci@gmail.com>
2022-11-24 22:48:18 +0300

    slider

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,27 +1,45 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined';
 import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
 import ListItem from '../listItem/ListItem';
 import './list.scss'
 
 function List() {
+  const [slideNumber, setSlideNumber] = useState(0);
+
+  const listRef = useRef();
+
+  const handleClick = (direction) => {
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (direction === 'left' && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1)
+      listRef.current.style.transform = `translateX(${230 + distance}px)`
+    }
+    if (direction === 'right' && slideNumber < 5) {
+      setSlideNumber(slideNumber + 1)
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`
+    }
+  }
 
   return (
     <div className='list'>
-        <span className="listTitle">Continue to watch</span>
-        <div className="wrapper">
-            <ArrowBackIosNewOutlinedIcon className='sliderArrow left'/>
-            <div className="container">
-                <ListItem />
-                <ListItem />
-                <ListItem />
-                <ListItem />
-                <ListItem />
-                <ListItem />
-                <ListItem />
-            </div>
-            <ArrowForwardIosOutlinedIcon className='sliderArrow right'/>
+      <span className="listTitle">Continue to watch</span>
+      <div className="wrapper">
+        <ArrowBackIosNewOutlinedIcon className='sliderArrow left' onClick={() => handleClick('left')} />
+        <div className="container" ref={listRef}>
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
         </div>
+        <ArrowForwardIosOutlinedIcon className='sliderArrow right' onClick={() => handleClick('right')} />
+      </div>
     </div>
   )
 }
