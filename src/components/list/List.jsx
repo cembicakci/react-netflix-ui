e13e3c814f92c93f9551a16fe6419c942732d086
Cem <cmbicakci@gmail.com>
2022-11-27 11:23:11 +0300

    slider icons

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -5,11 +5,13 @@ import ListItem from '../listItem/ListItem';
 import './list.scss'
 
 function List() {
+  const [isMoved, setIsMoved] = useState(false);
   const [slideNumber, setSlideNumber] = useState(0);
 
   const listRef = useRef();
 
   const handleClick = (direction) => {
+    setIsMoved(true)
     let distance = listRef.current.getBoundingClientRect().x - 50;
     if (direction === 'left' && slideNumber > 0) {
       setSlideNumber(slideNumber - 1)
@@ -25,7 +27,7 @@ function List() {
     <div className='list'>
       <span className="listTitle">Continue to watch</span>
       <div className="wrapper">
-        <ArrowBackIosNewOutlinedIcon className='sliderArrow left' onClick={() => handleClick('left')} />
+        <ArrowBackIosNewOutlinedIcon className='sliderArrow left' onClick={() => handleClick('left')} style={{ display: !isMoved && 'none' }} />
         <div className="container" ref={listRef}>
           <ListItem />
           <ListItem />
