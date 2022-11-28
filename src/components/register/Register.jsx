adc741b307884268c40e4a06ba27b84047d8b6c2
Cem <cmbicakci@gmail.com>
2022-11-28 19:40:18 +0300

    useRef

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './register.scss'
 
 function Register() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    const handleStart = () => {
+        setEmail(emailRef.current.value);
+    }
+
+    const handleFinish = () => {
+        setPassword(passwordRef.current.value);
+
+    }
+
+    console.log(email, password)
+
     return (
         <div className='register'>
             <div className="top">
@@ -20,10 +38,21 @@ function Register() {
                 <p>
                     Ready to watch? Enter your email to create or restart your membership.
                 </p>
-                <div className="input">
-                    <input type="email" placeholder='email address' />
-                    <button className='registerButton'>Get Started</button>
-                </div>
+
+                {
+                    !email ? (
+                        <div className="input">
+                            <input type="email" placeholder='email address' ref={emailRef} />
+                            <button className='registerButton' onClick={handleStart}>Get Started</button>
+                        </div>
+                    ) : (
+                        <form className="input">
+                            <input type="password" placeholder='password' ref={passwordRef} />
+                            <button className='registerButton' onClick={handleFinish}>Start</button>
+                        </form>
+                    )
+                }
+
             </div>
         </div>
     )
