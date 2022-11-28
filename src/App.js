c324f568d27329bdaa65c179ad7ff2b411e0dbc7
Cem <cmbicakci@gmail.com>
2022-11-28 19:31:20 +0300

    register page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.scss';
 import Home from './pages/home/Home';
 import Watch from './pages/watch/Watch';
+import Register from './components/register/Register';
 
 function App() {
   return (
     <div className="App">
-      <Watch />
+      <Register />
     </div>
   );
 }
